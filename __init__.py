8dd8aff1308f41de59930596091b0e4eee2579ab
6opcyk <podobriy11@mail.ua>
2018-05-07 21:12:28 +0300

    Revert "First Commit"
    This reverts commit 1d5c4adc30174f7869c8c35c1b5472d126a639a0.

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8-*-
-from PyQt5.QtCore import *
-from MainWindow import *
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    mainWin = MainWindow()
-    sys.exit(app.exec())
